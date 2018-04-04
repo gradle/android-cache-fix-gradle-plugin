925ce622fd6e24fdca354a3e53278549d7dfe6de
Lóránt Pintér <lorant@gradle.com>
2018-04-04 12:31:37 +0200

    Upgrade to Android 3.2.0-alpha09
    Still failing

--- src/test/groovy/org/gradle/android/PluginApplicationTest.groovy
+++ src/test/groovy/org/gradle/android/PluginApplicationTest.groovy
@@ -48,8 +48,8 @@ class PluginApplicationTest extends AbstractTest {
         false | false       | "3.0.1"
         false | true        | "3.1.0-beta3"
         true  | false       | "3.1.0-beta3"
-        false | true        | "3.2.0-alpha08"
-        true  | false       | "3.2.0-alpha08"
+        false | true        | "3.2.0-alpha09"
+        true  | false       | "3.2.0-alpha09"
         description = warns ? "warn" : "not warn"
     }
 }

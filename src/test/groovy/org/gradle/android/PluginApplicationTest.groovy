1781c04740b0840ed6eb26728bdacec91cca7882
Lóránt Pintér <lorant@gradle.com>
2018-04-09 19:43:09 +0200

    Sopport Android 3.1.1

--- src/test/groovy/org/gradle/android/PluginApplicationTest.groovy
+++ src/test/groovy/org/gradle/android/PluginApplicationTest.groovy
@@ -48,6 +48,8 @@ class PluginApplicationTest extends AbstractTest {
         false | false       | "3.0.1"
         false | true        | "3.1.0"
         true  | false       | "3.1.0"
+        false | true        | "3.1.1"
+        true  | false       | "3.1.1"
         false | true        | "3.2.0-alpha01"
         true  | false       | "3.2.0-alpha01"
         description = warns ? "warn" : "not warn"

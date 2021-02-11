7f818be3d146e758a38eaa21887127a66b3de737
Nelson Osacky <nosacky@gradle.com>
2021-02-11 15:19:35 +0100

    Disable caching for MergeNativeLibs.
    This disables caching for the MergeNativeLibs task because it behaves like a copy task which do not benefit from caching.
    
    This PR also removes the workaround for DexFileDependencies since this task is not cacheable by default.

--- src/main/groovy/org/gradle/android/workarounds/MergeNativeLibsWorkaround.groovy
+++ src/main/groovy/org/gradle/android/workarounds/MergeNativeLibsWorkaround.groovy
@@ -2,37 +2,19 @@ package org.gradle.android.workarounds
 
 import com.android.build.gradle.internal.tasks.MergeNativeLibsTask
 import org.gradle.android.AndroidIssue
-import org.gradle.api.Task
-import org.gradle.api.file.FileCollection
+import org.gradle.api.Project
 
-import java.lang.reflect.Field
-
-/**
- * Fixes the cacheability issue with MergeNativeLibsTask where the projectNativeLibs field is treated as an input with
- * absolute path sensitivity.  This mostly comes up when either native libraries are built with the application
- * or RenderScript files are compiled as part of the build.
- *
- * In 3.5.x, projectNativeLibs is not directly backed by state in the Task object.  Instead, it is effectively a
- * method that calculates a new FileCollection from variantScope every time it is called.  So we only
- * support this workaround for 3.6.x and 4.0.x.
- */
-@AndroidIssue(introducedIn = "3.6.0", fixedIn = ["4.1.0-alpha09"], link = "https://issuetracker.google.com/issues/140602655")
-class MergeNativeLibsWorkaround extends AbstractAbsolutePathWorkaround {
-    Class<?> androidTaskClass = MergeNativeLibsTask.class
-    String propertyName = "projectNativeLibs"
-
+@AndroidIssue(introducedIn = "3.5.0", fixedIn = [], link = "https://issuetracker.google.com/issues/153088766")
+class MergeNativeLibsWorkaround implements Workaround {
     @Override
     void apply(WorkaroundContext context) {
-        super.apply(context)
+        context.project.tasks.withType(MergeNativeLibsTask).configureEach {
+            outputs.doNotCacheIf("Caching MergeNativeLibs adds overhead to the build and never saves time", {true })
+        }
     }
 
-    public void setPropertyValue(Task task, FileCollection fileCollection) {
-        // The projectNativeLibs input is a decorated field without a setter in 3.6.x and 4.0.x
-        Field field = task.class.getDeclaredFields().find { it.name == "__${propertyName}__" }
-
-        if (field != null) {
-            field.setAccessible(true)
-            field.set(task, fileCollection)
-        }
+    @Override
+    boolean canBeApplied(Project project) {
+        return true
     }
 }

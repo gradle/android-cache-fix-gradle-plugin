5b2750a5d0013bbe60ff280e1c81a0745bdc165c
Daz DeBoer <daz@gradle.com>
2021-02-11 12:22:22 -0700

    Add test for configuration caching

---
+++ src/test/groovy/org/gradle/android/ConfigurationCachingTest.groovy
@@ -0,0 +1,21 @@
+package org.gradle.android
+
+class ConfigurationCachingTest extends AbstractTest {
+    def "Can run with configuration cache"() {
+        given:
+        SimpleAndroidApp.builder(temporaryFolder.root, cacheDir)
+                .withAndroidVersion(Versions.latestAndroidVersion())
+                .withKotlinDisabled()
+                .build()
+                .writeProject()
+
+        when:
+        def result = withGradleVersion(Versions.latestGradleVersion().version)
+                .withProjectDir(temporaryFolder.root)
+                .withArguments('--configuration-cache', '--configuration-cache-problems=warn', 'assembleDebug')
+                .build()
+
+        then:
+        result.output.contains("14 problems were found storing the configuration cache")
+    }
+}

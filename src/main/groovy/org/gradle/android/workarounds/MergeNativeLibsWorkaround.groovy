61c37051d954872f423c55a880af57864f62d249
Nelson Osacky <nosacky@gradle.com>
2021-02-16 16:09:50 +0100

    Update doNotCache message.

--- src/main/groovy/org/gradle/android/workarounds/MergeNativeLibsWorkaround.groovy
+++ src/main/groovy/org/gradle/android/workarounds/MergeNativeLibsWorkaround.groovy
@@ -9,7 +9,7 @@ class MergeNativeLibsWorkaround implements Workaround {
     @Override
     void apply(WorkaroundContext context) {
         context.project.tasks.withType(MergeNativeLibsTask).configureEach {
-            outputs.doNotCacheIf("Caching MergeNativeLibs adds overhead to the build and never saves time", {true })
+            outputs.doNotCacheIf("Caching MergeNativeLibs is unlikely to provide positive performance results.", {true })
         }
     }
 

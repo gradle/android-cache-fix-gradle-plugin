16816df283d11fb288ef0c3fbdf31328ea9e955e
Gary Hale <gary@gradle.com>
2021-02-23 09:08:49 -0500

    Fix unfinished javadoc comment

--- src/test/groovy/org/gradle/android/MultiVersionTest.groovy
+++ src/test/groovy/org/gradle/android/MultiVersionTest.groovy
@@ -1,7 +1,8 @@
 package org.gradle.android
 
 /**
- * Represents tests that should be run with
+ * Represents tests that span multiple versions of Android Gradle Plugin and need to be executed
+ * with multiple versions of the JDK.
  */
 interface MultiVersionTest {
 

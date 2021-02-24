d5c52f811a2fffcd77530ead17c3092701c676b3
Gary Hale <gary@gradle.com>
2021-02-24 14:29:59 -0500

    Update workaround test for latest beta of AGP 4.2

--- src/test/groovy/org/gradle/android/WorkaroundTest.groovy
+++ src/test/groovy/org/gradle/android/WorkaroundTest.groovy
@@ -13,7 +13,7 @@ class WorkaroundTest extends Specification {
         where:
         androidVersion  | expectedWorkarounds
         "7.0.0-alpha01" | ['RoomSchemaLocation', 'CompileLibraryResources_4_2', 'MergeResources', 'MergeNativeLibs']
-        "4.2.0-beta04"  | ['RoomSchemaLocation', 'CompileLibraryResources_4_2', 'MergeResources', 'MergeNativeLibs']
+        "4.2.0-beta05"  | ['RoomSchemaLocation', 'CompileLibraryResources_4_2', 'MergeResources', 'MergeNativeLibs']
         "4.1.2"         | ['RoomSchemaLocation', 'CompileLibraryResources_4_0', 'MergeResources', 'MergeNativeLibs']
         "4.0.2"         | ['MergeJavaResources', 'MergeNativeLibs', 'RoomSchemaLocation', 'CompileLibraryResources_4_0', 'MergeResources']
         "3.6.4"         | ['MergeJavaResources', 'MergeNativeLibs', 'RoomSchemaLocation']

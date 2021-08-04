d46d4b5c609feea1adb01c8806de1a7ff48fe628
Gary Hale <gary@gradle.com>
2021-08-04 17:17:20 -0400

    Add test to catch room merge issue

--- src/test/groovy/org/gradle/android/RoomSchemaLocationWorkaroundTest.groovy
+++ src/test/groovy/org/gradle/android/RoomSchemaLocationWorkaroundTest.groovy
@@ -13,6 +13,7 @@ import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 @MultiVersionTest
 class RoomSchemaLocationWorkaroundTest extends AbstractTest {
     private static final String[] CLEAN_BUILD = ["clean", "testDebug", "testRelease", "assembleAndroidTest", "--build-cache", "--stacktrace"]
+    private static final String[] INCREMENTAL_DEBUG_BUILD = ["assembleDebug", "--build-cache", "--stacktrace"]
     private static final List<String> ALL_PROJECTS = ["app", "library"]
     private static final List<String> ALL_VARIANTS = ["debug", "release"]
 
@@ -192,6 +193,9 @@ class RoomSchemaLocationWorkaroundTest extends AbstractTest {
         and:
         assertMergedSchemaOutputsExist()
 
+        and:
+
+
         where:
         androidVersion << TestVersions.latestAndroidVersions
     }
@@ -237,6 +241,72 @@ class RoomSchemaLocationWorkaroundTest extends AbstractTest {
         where:
         kotlinVersion << [ "1.3.61", "1.3.50" ].collect { VersionNumber.parse(it) }
     }
+
+    @Unroll
+    def "schemas are correctly generated when only one variant is built incrementally (Android #androidVersion)"() {
+        SimpleAndroidApp.builder(temporaryFolder.root, cacheDir)
+            .withAndroidVersion(androidVersion)
+            .withKotlinVersion(VersionNumber.parse("1.5.0"))
+            .build()
+            .writeProject()
+
+        cacheDir.deleteDir()
+        cacheDir.mkdirs()
+
+        when:
+        BuildResult buildResult = withGradleVersion(TestVersions.latestSupportedGradleVersionFor(androidVersion).version)
+            .forwardOutput()
+            .withProjectDir(temporaryFolder.root)
+            .withArguments(CLEAN_BUILD)
+            .build()
+
+        then:
+        assertCompileTasksHaveOutcome(buildResult, SUCCESS)
+        assertCompileAndroidTestTasksHaveOutcome(buildResult, SUCCESS)
+        assertCompileUnitTestTasksHaveOutcome(buildResult, SUCCESS)
+        assertKaptTasksHaveOutcome(buildResult, SUCCESS)
+        assertKaptAndroidTestTasksHaveOutcome(buildResult, SUCCESS)
+        assertKaptUnitTestTasksHaveOutcome(buildResult, SUCCESS)
+        buildResult.task(':app:mergeRoomSchemaLocations').outcome == SUCCESS
+        buildResult.task(':library:mergeRoomSchemaLocations').outcome == SUCCESS
+
+        and:
+        assertKaptSchemaOutputsExist()
+
+        and:
+        assertMergedSchemaOutputsExist()
+
+        when:
+        modifyRoomColumnName("last_update", "foo")
+        buildResult = withGradleVersion(TestVersions.latestSupportedGradleVersionFor(androidVersion).version)
+            .forwardOutput()
+            .withProjectDir(temporaryFolder.root)
+            .withArguments(INCREMENTAL_DEBUG_BUILD)
+            .build()
+
+        then:
+        assertCompileTasksHaveOutcome(buildResult, SUCCESS, ["debug"])
+        assertKaptTasksHaveOutcome(buildResult, SUCCESS, ["debug"])
+        buildResult.task(':app:mergeRoomSchemaLocations').outcome == SUCCESS
+        buildResult.task(':library:mergeRoomSchemaLocations').outcome == SUCCESS
+
+        and:
+        assertKaptSchemaOutputsExist()
+
+        and:
+        assertMergedSchemaOutputsExist()
+
+        and:
+        assertKaptSchemaContainsColumnFor('debug', 'foo')
+
+        and:
+        assertMergedRoomSchemaContainsColumn("foo")
+
+        where:
+        //noinspection GroovyAssignabilityCheck
+        androidVersion << TestVersions.latestAndroidVersions
+    }
+
 
     def "workaround throws an exception when room extension is not configured, but annotation processor argument is"() {
         def androidVersion = TestVersions.latestAndroidVersionForCurrentJDK()
@@ -317,32 +387,28 @@ class RoomSchemaLocationWorkaroundTest extends AbstractTest {
         assert !buildResult.tasks.collect {it.path }.contains(taskPath)
     }
 
-    void assertCompileTasksHaveOutcome(BuildResult buildResult, TaskOutcome outcome) {
-        assertAllVariantTasksHaveOutcome(buildResult, outcome) { project, variant -> ":${project}:compile${variant.capitalize()}JavaWithJavac" }
+    void assertCompileTasksHaveOutcome(BuildResult buildResult, TaskOutcome outcome, List<String> variants = ALL_VARIANTS) {
+        assertAllVariantTasksHaveOutcome(buildResult, outcome, ALL_PROJECTS, variants) { project, variant -> ":${project}:compile${variant.capitalize()}JavaWithJavac" }
     }
 
     void assertCompileAndroidTestTasksHaveOutcome(BuildResult buildResult, TaskOutcome outcome) {
         assertAllVariantTasksHaveOutcome(buildResult, outcome, ALL_PROJECTS, ["debug"]) { project, variant -> ":${project}:compile${variant.capitalize()}AndroidTestJavaWithJavac" }
     }
 
-    void assertCompileUnitTestTasksHaveOutcome(BuildResult buildResult, TaskOutcome outcome) {
-        assertAllVariantTasksHaveOutcome(buildResult, outcome) { project, variant -> ":${project}:compile${variant.capitalize()}UnitTestJavaWithJavac" }
+    void assertCompileUnitTestTasksHaveOutcome(BuildResult buildResult, TaskOutcome outcome, List<String> variants = ALL_VARIANTS) {
+        assertAllVariantTasksHaveOutcome(buildResult, outcome, ALL_PROJECTS, variants) { project, variant -> ":${project}:compile${variant.capitalize()}UnitTestJavaWithJavac" }
     }
 
-    void assertKaptTasksHaveOutcome(BuildResult buildResult, TaskOutcome outcome) {
-        assertAllVariantTasksHaveOutcome(buildResult, outcome) { project, variant -> ":${project}:kapt${variant.capitalize()}Kotlin" }
+    void assertKaptTasksHaveOutcome(BuildResult buildResult, TaskOutcome outcome, List<String> variants = ALL_VARIANTS) {
+        assertAllVariantTasksHaveOutcome(buildResult, outcome, ALL_PROJECTS, variants) { project, variant -> ":${project}:kapt${variant.capitalize()}Kotlin" }
     }
 
     void assertKaptAndroidTestTasksHaveOutcome(BuildResult buildResult, TaskOutcome outcome) {
         assertAllVariantTasksHaveOutcome(buildResult, outcome, ALL_PROJECTS, ["debug"]) { project, variant -> ":${project}:kapt${variant.capitalize()}AndroidTestKotlin" }
     }
 
-    void assertKaptUnitTestTasksHaveOutcome(BuildResult buildResult, TaskOutcome outcome) {
-        assertAllVariantTasksHaveOutcome(buildResult, outcome) { project, variant -> ":${project}:kapt${variant.capitalize()}UnitTestKotlin" }
-    }
-
-    void assertAllVariantTasksHaveOutcome(BuildResult buildResult, TaskOutcome taskOutcome, Closure<String> taskPathTransform) {
-        assertAllVariantTasksHaveOutcome(buildResult, taskOutcome, ALL_PROJECTS, ALL_VARIANTS, taskPathTransform)
+    void assertKaptUnitTestTasksHaveOutcome(BuildResult buildResult, TaskOutcome outcome, List<String> variants = ALL_VARIANTS) {
+        assertAllVariantTasksHaveOutcome(buildResult, outcome, ALL_PROJECTS, variants) { project, variant -> ":${project}:kapt${variant.capitalize()}UnitTestKotlin" }
     }
 
     void assertAllVariantTasksHaveOutcome(BuildResult buildResult, TaskOutcome taskOutcome, List<String> projects, List<String> variants, Closure<String> taskPathTransform) {
@@ -382,7 +448,36 @@ class RoomSchemaLocationWorkaroundTest extends AbstractTest {
     }
 
     void assertSchemasExist(String project, String baseDirPath) {
-        assert file("${project}/${baseDirPath}/org.gradle.android.example.${project}.AppDatabase/1.json").exists()
-        assert file("${project}/${baseDirPath}/org.gradle.android.example.${project}.AppDatabase/2.json").exists()
+        assert file("${roomSchemaDirPath(project, baseDirPath)}/1.json").exists()
+        assert file("${roomSchemaDirPath(project, baseDirPath)}/2.json").exists()
+    }
+
+    static String roomSchemaDirPath(String project, String baseDirPath) {
+        return "${project}/${baseDirPath}/org.gradle.android.example.${project}.AppDatabase"
+    }
+
+    void modifyRoomColumnName(String oldColumnName, String newColumnName) {
+        modifyRoomColumnName("app", oldColumnName, newColumnName)
+        modifyRoomColumnName("library", oldColumnName, newColumnName)
+    }
+
+    void assertKaptSchemaContainsColumnFor(String columnName, String variant) {
+        assertRoomSchemaContainsColumn("app", "build/roomSchemas/kapt${variant.capitalize()}Kotlin", columnName)
+        assertRoomSchemaContainsColumn("library", "build/roomSchemas/kapt${variant.capitalize()}Kotlin", columnName)
+    }
+
+    void assertMergedRoomSchemaContainsColumn(String columnName) {
+        assertRoomSchemaContainsColumn("app", 'schemas', columnName)
+        assertRoomSchemaContainsColumn("library", 'schemas', columnName)
+    }
+
+    void assertRoomSchemaContainsColumn(String project, String baseDirPath, String columnName) {
+        assert file("${roomSchemaDirPath(project, 'schemas')}/2.json").text.contains("\"columnName\": \"${columnName}\",")
+    }
+
+    void modifyRoomColumnName(String project, String oldColumnName, String newColumnName) {
+        def schemaSourceFile = file("${project}/src/main/java/org/gradle/android/example/${project}/AppDatabase.java")
+        schemaSourceFile.text = schemaSourceFile.text.replaceAll("ADD COLUMN ${oldColumnName}", "ADD COLUMN ${newColumnName}")
+        assert schemaSourceFile.text.contains("ADD COLUMN ${newColumnName}")
     }
 }

550dbd9e3d0df516ff8207026f43afc2afbbb9c8
Alexis Tual <atual@gradle.com>
2021-09-24 09:38:40 +0200

    Disable caching of DataBindingMergeDependencyArtifacts task
    Signed-off-by: Alexis Tual <atual@gradle.com>

---
+++ src/main/groovy/org/gradle/android/workarounds/DataBindingMergeDependencyArtifactsWorkaround.groovy
@@ -0,0 +1,28 @@
+package org.gradle.android.workarounds
+
+import com.android.build.gradle.internal.tasks.databinding.DataBindingMergeDependencyArtifactsTask
+import groovy.transform.CompileStatic
+import org.gradle.android.AndroidIssue
+import org.gradle.api.Project
+
+/**
+ * Disables caching of the DataBindingMergeDependencyArtifactsTask task which is mostly disk bound and
+ * unlikely to provide positive performance benefits.
+ */
+@AndroidIssue(introducedIn = "3.5.0", fixedIn = [], link = "https://issuetracker.google.com/issues/200002454")
+@CompileStatic
+class DataBindingMergeDependencyArtifactsWorkaround implements Workaround {
+    private static final String CACHING_ENABLED_PROPERTY = "org.gradle.android.cache-fix.DataBindingMergeDependencyArtifacts.caching.enabled"
+
+    @Override
+    void apply(WorkaroundContext context) {
+        context.project.tasks.withType(DataBindingMergeDependencyArtifactsTask).configureEach {
+            it.outputs.doNotCacheIf("Caching DataBindingMergeDependencyArtifacts is unlikely to provide positive performance results.", { true })
+        }
+    }
+
+    @Override
+    boolean canBeApplied(Project project) {
+        return !SystemPropertiesCompat.getBoolean(CACHING_ENABLED_PROPERTY, project)
+    }
+}

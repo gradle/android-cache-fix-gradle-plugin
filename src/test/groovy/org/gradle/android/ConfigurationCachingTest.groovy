a11ba045e12c48626037458b3ae7d2b3e93e643e
Nelson Osacky <nosacky@gradle.com>
2022-03-15 14:32:57 +0100

    Update to AGP 7.3-alpha06
    Also make configuration cache assertion clear

--- src/test/groovy/org/gradle/android/ConfigurationCachingTest.groovy
+++ src/test/groovy/org/gradle/android/ConfigurationCachingTest.groovy
@@ -6,6 +6,7 @@ import org.gradle.util.VersionNumber
 @MultiVersionTest
 class ConfigurationCachingTest extends AbstractTest {
     private static final VersionNumber SUPPORTED_KOTLIN_VERSION = TestVersions.latestSupportedKotlinVersion()
+    static final String CC_PROBLEMS_FOUND = "problems were found storing the configuration cache"
 
     def "plugin is compatible with configuration cache"() {
         given:
@@ -22,10 +23,7 @@ class ConfigurationCachingTest extends AbstractTest {
                 .build()
 
         then:
-        // In Gradle 7.4 the build always uses the following words.
-        if (!result.output.contains("0 problems were found storing the configuration cache")) {
-            !result.output.contains("problems were found storing the configuration cache")
-        }
+        assertNoConfigCacheProblemsFound(result)
 
         when:
         result = withGradleVersion(TestVersions.latestGradleVersion().version)
@@ -39,5 +37,9 @@ class ConfigurationCachingTest extends AbstractTest {
 
     void assertConfigurationCacheIsReused(BuildResult result) {
         assert result.output.contains('Reusing configuration cache.')
+    }
+
+    void assertNoConfigCacheProblemsFound(BuildResult result) {
+        assert !result.output.contains(CC_PROBLEMS_FOUND) || result.output.contains("0 ${CC_PROBLEMS_FOUND}")
     }
 }

2f6720b9643897517325ae985bfcddd5693dd77a
Inaki Villar <inaki.seri@gmail.com>
2022-07-25 18:00:13 -0700

    adding property flag for RoomSchemaLocationWorkaround

--- src/main/groovy/org/gradle/android/workarounds/RoomSchemaLocationWorkaround.groovy
+++ src/main/groovy/org/gradle/android/workarounds/RoomSchemaLocationWorkaround.groovy
@@ -51,6 +51,7 @@ import java.util.function.Supplier
  */
 @AndroidIssue(introducedIn = "3.5.0", fixedIn = [], link = "https://issuetracker.google.com/issues/132245929")
 class RoomSchemaLocationWorkaround implements Workaround {
+    public static final String WORKAROUND_ENABLED_PROPERTY = "org.gradle.android.cache-fix.RoomSchemaLocationWorkaround.enabled"
     public static final String ROOM_SCHEMA_LOCATION = "room.schemaLocation"
     private static final VersionNumber MINIMUM_KOTLIN_VERSION = VersionNumber.parse("1.4.32")
     private static final VersionNumber KOTLIN_VERSION = getKotlinVersion()
@@ -58,10 +59,11 @@ class RoomSchemaLocationWorkaround implements Workaround {
     @Override
     boolean canBeApplied(Project project) {
         if (KOTLIN_VERSION != VersionNumber.UNKNOWN && KOTLIN_VERSION < MINIMUM_KOTLIN_VERSION) {
+            SystemPropertiesCompat.getBoolean(WORKAROUND_ENABLED_PROPERTY, project, true)
             project.logger.info("${this.class.simpleName} is only compatible with Kotlin Gradle plugin version 1.4.32 or higher (found ${KOTLIN_VERSION.toString()}).")
             return false
         } else {
-            return true
+            return SystemPropertiesCompat.getBoolean(WORKAROUND_ENABLED_PROPERTY, project, true)
         }
     }
 

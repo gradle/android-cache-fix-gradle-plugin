8418d9aa0f75b677e63fe9abd447c3a74e27b6c2
Inaki Villar <inaki.seri@gmail.com>
2022-12-23 15:07:11 -0800

    Update Kotlin supported versions with 1.7.21

--- src/test/groovy/org/gradle/android/RoomSchemaLocationWorkaroundTest.groovy
+++ src/test/groovy/org/gradle/android/RoomSchemaLocationWorkaroundTest.groovy
@@ -21,7 +21,7 @@ class RoomSchemaLocationWorkaroundTest extends AbstractTest {
     def "schemas are generated into task-specific directory and are cacheable with kotlin and kapt workers enabled (Android #androidVersion) (Kotlin #kotlinVersion)"() {
         def kotlinVersionNumber = VersionNumber.parse(kotlinVersion)
         // There are kotlin module version errors when using older versions of kotlin with AGP 7.2.0+ in this configuration
-        Assume.assumeFalse(androidVersion >= VersionNumber.parse("7.2.0-alpha01") && kotlinVersionNumber < VersionNumber.parse("1.5.0"))
+        Assume.assumeFalse(androidVersion >= VersionNumber.parse("7.2.0-alpha01") && kotlinVersionNumber < VersionNumber.parse("1.6.0"))
 
         SimpleAndroidApp.builder(temporaryFolder.root, cacheDir)
             .withAndroidVersion(androidVersion)
@@ -463,7 +463,7 @@ class RoomSchemaLocationWorkaroundTest extends AbstractTest {
     def "does not error when tasks are eagerly created (Android #androidVersion) (Kotlin #kotlinVersion)"() {
         def kotlinVersionNumber = VersionNumber.parse(kotlinVersion)
         // There are kotlin module version errors when using older versions of kotlin with AGP 7.2.0+ in this configuration
-        Assume.assumeFalse(androidVersion >= VersionNumber.parse("7.2.0-alpha01") && kotlinVersionNumber < VersionNumber.parse("1.5.0"))
+        Assume.assumeFalse(androidVersion >= VersionNumber.parse("7.2.0-alpha01") && kotlinVersionNumber < VersionNumber.parse("1.6.0"))
 
         SimpleAndroidApp.builder(temporaryFolder.root, cacheDir)
             .withAndroidVersion(androidVersion)

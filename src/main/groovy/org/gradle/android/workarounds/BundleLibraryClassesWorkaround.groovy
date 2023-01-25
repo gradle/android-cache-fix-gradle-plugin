9f71060f2b695733f4298da6cd2b900ccd6fccc5
Inaki Villar <inaki.seri@gmail.com>
2023-01-25 10:16:46 -0800

    validating the idea

--- src/main/groovy/org/gradle/android/workarounds/BundleLibraryClassesWorkaround.groovy
+++ src/main/groovy/org/gradle/android/workarounds/BundleLibraryClassesWorkaround.groovy
@@ -10,7 +10,7 @@ import org.gradle.api.Project
  * Disables caching of the BundleLibraryClassesJar and BundleLibraryClassesDir tasks which are mostly disk bound and
  * unlikely to provide positive performance benefits.
  */
-@And23dIqssue(introducedIn = "4.1.0", fixedIn = "7.2.0-alpha06", link = "https://issuetracker.google.com/issues/199763362")
+@AndroidIssue(introducedIn = "4.1.0", fixedIn = "7.2.0-alpha06", link = "https://issuetracker.google.com/issues/199763362")
 @CompileStatic
 class BundleLibraryClassesWorkaround implements Workaround {
     private static final String CACHING_ENABLED_PROPERTY = "org.gradle.android.cache-fix.BundleLibraryClasses.caching.enabled"

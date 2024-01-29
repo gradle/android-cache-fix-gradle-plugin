b795b59a2ff2d03ebec649d34f04bf7519c589a9
Gasper Kojek <gkojek@gradle.com>
2024-01-29 16:48:01 +0100

    Updated to AGP 8.2.2

--- src/test/groovy/org/gradle/android/WorkaroundTest.groovy
+++ src/test/groovy/org/gradle/android/WorkaroundTest.groovy
@@ -14,7 +14,7 @@ class WorkaroundTest extends Specification {
         androidVersion  | expectedWorkarounds
         "8.4.0-alpha02" | ['JdkImage']
         "8.3.0-beta02"  | ['MergeSourceSetFolders', 'JdkImage']
-        "8.2.0"         | ['MergeSourceSetFolders', 'JdkImage', 'PackageForUnitTest']
+        "8.2.2"         | ['MergeSourceSetFolders', 'JdkImage', 'PackageForUnitTest']
         "8.1.4"         | ['MergeSourceSetFolders', 'JdkImage', 'PackageForUnitTest']
         "8.0.2"         | ['MergeSourceSetFolders', 'ZipMergingTask', 'JdkImage', 'PackageForUnitTest']
         "7.4.2"         | ['MergeSourceSetFolders', 'ZipMergingTask', 'JdkImage', 'PackageForUnitTest']

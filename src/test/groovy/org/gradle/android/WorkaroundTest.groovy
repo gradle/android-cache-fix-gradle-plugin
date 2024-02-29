4cba04de9d4a4a2b855945e7405b86dce53672b6
Inaki Villar <inaki.seri@gmail.com>
2024-02-29 14:06:33 -0800

    update AGP 8.3.0

--- src/test/groovy/org/gradle/android/WorkaroundTest.groovy
+++ src/test/groovy/org/gradle/android/WorkaroundTest.groovy
@@ -13,7 +13,7 @@ class WorkaroundTest extends Specification {
         where:
         androidVersion  | expectedWorkarounds
         "8.4.0-alpha11" | ['JdkImage']
-        "8.3.0-rc02"    | ['MergeSourceSetFolders', 'JdkImage']
+        "8.3.0"         | ['MergeSourceSetFolders', 'JdkImage']
         "8.2.2"         | ['MergeSourceSetFolders', 'JdkImage', 'PackageForUnitTest']
         "8.1.4"         | ['MergeSourceSetFolders', 'JdkImage', 'PackageForUnitTest']
         "8.0.2"         | ['MergeSourceSetFolders', 'ZipMergingTask', 'JdkImage', 'PackageForUnitTest']

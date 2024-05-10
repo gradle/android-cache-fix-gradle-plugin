948964d7d57ec47afe70b296ad79c4230894fe83
Eric Haag <ehaag@gradle.com>
2024-05-10 13:14:04 -0500

    Bump tested AGP version from 8.5.0-alpha08 to 8.5.0-beta01

--- src/test/groovy/org/gradle/android/WorkaroundTest.groovy
+++ src/test/groovy/org/gradle/android/WorkaroundTest.groovy
@@ -12,7 +12,7 @@ class WorkaroundTest extends Specification {
         workarounds.collect { it.class.simpleName.replaceAll(/Workaround/, "") }.sort() == expectedWorkarounds.sort()
         where:
         androidVersion  | expectedWorkarounds
-        "8.5.0-alpha08" | ['JdkImage']
+        "8.5.0-beta01"  | ['JdkImage']
         "8.4.0"         | ['JdkImage']
         "8.3.2"         | ['MergeSourceSetFolders', 'JdkImage']
         "8.2.2"         | ['MergeSourceSetFolders', 'JdkImage', 'PackageForUnitTest']

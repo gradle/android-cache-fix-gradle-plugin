53e74abca875f3f1ff1873032712560cc69f1900
Iurii Ignatko <iignatko@gradle.com>
2024-10-28 16:18:32 +0100

    Add additional tags to the published pom

--- build.gradle.kts
+++ build.gradle.kts
@@ -104,6 +104,28 @@ publishing {
     publications {
         create<MavenPublication>("pluginMaven") {
             groupId = pluginGroupId
+            pom {
+                name.set("Common Custom User Data Gradle Plugin")
+                description.set("A Gradle Plugin to capture common custom user data used for Gradle Build Scans in Develocity")
+                url.set("https://github.com/gradle/common-custom-user-data-gradle-plugin")
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("The Gradle team")
+                        organization.set("Gradle Inc.")
+                        organizationUrl.set("https://gradle.com")
+                    }
+                }
+                scm {
+                    developerConnection.set("scm:git:https://github.com/gradle/common-custom-user-data-gradle-plugin.git")
+                    url.set("https://github.com/gradle/common-custom-user-data-gradle-plugin")
+                }
+            }
         }
     }
     repositories {
